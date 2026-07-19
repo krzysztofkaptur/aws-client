76b69f81a4c81c59958884b8b72a945177fd95ff
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2026-07-19 19:13:43 +0200

    test deploy

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -4,6 +4,6 @@ import Home from './Home'
 describe('Home', () => {
   it('renders the page heading', () => {
     render(<Home />)
-    expect(screen.getByRole('heading', { name: 'Home' })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'Home edited' })).toBeInTheDocument()
   })
 })
